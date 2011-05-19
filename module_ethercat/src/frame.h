7f1dd468acfd63602f3f2363f789b47ce30af5ba
Henk Muller <henk@xmos.com>
2011-05-19 15:01:06 +0100

    Added copyrights

--- module_ethercat/src/frame.h
+++ module_ethercat/src/frame.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2011, XMOS Ltd, All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
 void rxProcess(in port rxActive, buffered in port:8 rxPort, streaming chanend toProtocol);
 void txProcess(buffered out port:8 txPort, streaming chanend fromProtocol);
 
--- module_ethercat/src/handler.h
+++ module_ethercat/src/handler.h
@@ -1,1 +1,6 @@
+// Copyright (c) 2011, XMOS Ltd, All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
 void frameProcess(streaming chanend fromRx, streaming chanend toTx);
